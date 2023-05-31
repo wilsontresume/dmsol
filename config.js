269b1732e0249b047a9b27b69adfb6ea64c76902
wilsonam89 <wilson.am@tresume.us>
2023-06-01 01:56:01 +0530

    First Commit

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+	debug: false,
+	database: {
+	    connectionLimit: 500,
+	    host: "217.21.85.154",
+	    user: "u922183766_whatsapp",
+	    password: "IlakkiyaWilson@123",
+	    database: "IlakkiyaWilson@123",
+	    charset : "utf8mb4",
+	    debug: false,
+	    waitForConnections: true,
+	    multipleStatements: true
+	},
+	cors: {
+		origin: '*',
+ 		optionsSuccessStatus: 200
+	}
+}
+
+module.exports = config; 
